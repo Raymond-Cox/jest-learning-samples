4bc7e7815481a1a0e87e9f75183afc5a7dcb9134
Raymond Cox <raymondc1911@gmail.com>
2023-06-27 17:11:47 -0400

    Removed unecessary comments from b-lesson-1 test file

--- src/functions/beginner/__tests__/b-lesson-1.test.js
+++ src/functions/beginner/__tests__/b-lesson-1.test.js
@@ -1,17 +1,6 @@
 import { add } from '../b-lesson-1'
 
-/**
- * An individual .test.js file is referred to as a test suite.
- * Each test suite is typically wrapped in a describe block
- * containing the name of the file.
- * This is a way of documenting the test suite for the terminal output.
- */
 describe('b-lesson-1', () => {
-    /**
-     * A test suite can contain multiple more describe blocks inside.
-     * Good practice is to create a new describe block for each
-     * function you want to write tests for.
-     */
     describe('add', () => {
         it('adds the numbers together correctly', () => {
             const result = add(5, 12)
